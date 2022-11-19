68facebf4ba002186e3bf0a60723c3773be920f7
AlvesBelem <marcelocartoes.pa@gmail.com>
2022-11-19 16:56:05 -0300

    criado middleware para validar token

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,9 @@
 import { Router, Response, Request } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { isAthenticaded } from "./middlewares/isAthenticaded";
 
 const router = Router();
 
@@ -8,5 +11,7 @@ const router = Router();
 router.post("/users", new CreateUserController().handle);
 
 router.post("/session", new AuthUserController().handle);
+
+router.get("/me", isAthenticaded, new DetailUserController().handle);
 
 export { router };
